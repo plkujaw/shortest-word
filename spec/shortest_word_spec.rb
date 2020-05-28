6b63063eed7da7f773c3d4637e8ee39408978495
plkujaw <jawor.jakub@gmail.com>
2020-05-28 08:51:15 +0100

    write and pass first test

--- spec/shortest_word_spec.rb
+++ spec/shortest_word_spec.rb
@@ -3,5 +3,6 @@ require 'shortest_word'
 describe '#shortest_word' do
   it 'returns length of the shortest word' do
     expect(shortest_word('kuba')).to eq(4)
+    expect(shortest_word('kuba j')).to eq(1)
   end
 end
